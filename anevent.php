20656c5cd38f9a12c3b1df1432f1743865a69d5f
Anthony Smith <smitha001@users.noreply.github.com>
2020-08-18 11:54:56 +1000

    Edits to improve
    Assumes unlimited capacity if capacity is not defined.
    Comments for campaign event endpoint And start date edits from end user.

--- anevent.php
+++ anevent.php
@@ -1,13 +1,18 @@
+    <?php  require_once $_SERVER['DOCUMENT_ROOT'].'/users/init.php'; ?>
+    <?php if (!securePage($_SERVER['PHP_SELF'])){die();} ?>    
 <?php
 $opts = array(
   'http'=>array(
     'method'=>"GET",
-    'header'=>"OSDI-API-Token: [put your action network api key here, and remove the square brackets]\r\n" 
+    'header'=>"OSDI-API-Token: d3345c20597af901f3002b1d3bbe4026\r\n" 
              
   )
 );
 $context = stream_context_create($opts);
-// Open the file using the HTTP headers set above
+
+
+
+// Recommend editing in 'https://actionnetwork.org/api/v2/event_campaigns/????????/events' in the line below, where ???????? is the campaign you are using...
 $file = file_get_contents('https://actionnetwork.org/api/v2/events/', false, $context);
 echo $file;
 ?>
